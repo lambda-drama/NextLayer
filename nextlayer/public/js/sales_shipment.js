09700be8d1bf778503faaa728d5c4d94bf25a947
maniamartial <martialmania19@gmail.com>
2025-08-27 22:00:57 +0300

    feat: Adding frontend

--- nextlayer/public/js/sales_shipment.js
+++ nextlayer/public/js/sales_shipment.js
@@ -7,60 +7,60 @@ frappe.ui.form.on("Sales Shipment Cost", {
 	},
 })
 
-// frappe.ui.form.on("Sales Landed Cost Taxes and Charges", {
-// 	refresh: function(frm){
-// 		frappe.throw(" I am here")
-// 	},
-//     expense_account: function (frm, cdt, cdn) {
-//         let row = locals[cdt][cdn];
-// 		frappe.throw("Uko")
-//         if (row.expense_account) {
-//             // Fetch account currency from backend
-//             frappe.db.get_value("Account", row.expense_account, "account_currency")
-//                 .then(r => {
-//                     if (!r.message) return;
-//                     let account_currency = r.message.account_currency;
-//                     let company_currency = frm.doc.company_currency;
+frappe.ui.form.on("Sales Landed Cost Taxes and Charges", {
+	refresh: function(frm){
+		frappe.throw(" I am here")
+	},
+    expense_account: function (frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+		frappe.throw("Uko")
+        if (row.expense_account) {
+            // Fetch account currency from backend
+            frappe.db.get_value("Account", row.expense_account, "account_currency")
+                .then(r => {
+                    if (!r.message) return;
+                    let account_currency = r.message.account_currency;
+                    let company_currency = frm.doc.company_currency;
 
-//                     if (account_currency && account_currency !== company_currency) {
-//                         // Call server to get exchange rate
-//                         frappe.call({
-//                             method: "nextlayer.next_layer.utils.fetch_exchange_rate",
-//                             args: {
-//                                 from_currency: account_currency,
-//                                 to_currency: company_currency,
-//                                 posting_date: frm.doc.posting_date
-//                             },
-//                             callback: function (res) {
-//                                 if (res.message) {
-//                                     frappe.model.set_value(cdt, cdn, "exchange_rate", res.message);
-//                                 }
-//                             }
-//                         });
-//                     } else {
-//                         frappe.model.set_value(cdt, cdn, "exchange_rate", 1);
-//                     }
-//                 });
-//         }
-//     },
+                    if (account_currency && account_currency !== company_currency) {
+                        // Call server to get exchange rate
+                        frappe.call({
+                            method: "nextlayer.next_layer.utils.fetch_exchange_rate",
+                            args: {
+                                from_currency: account_currency,
+                                to_currency: company_currency,
+                                posting_date: frm.doc.posting_date
+                            },
+                            callback: function (res) {
+                                if (res.message) {
+                                    frappe.model.set_value(cdt, cdn, "exchange_rate", res.message);
+                                }
+                            }
+                        });
+                    } else {
+                        frappe.model.set_value(cdt, cdn, "exchange_rate", 1);
+                    }
+                });
+        }
+    },
 
-//     amount: function (frm, cdt, cdn) {
-//         let row = locals[cdt][cdn];
-//         let company_currency = frm.doc.company_currency;
+    amount: function (frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        let company_currency = frm.doc.company_currency;
 
-//         if (row.amount && row.expense_account) {
-//             frappe.db.get_value("Account", row.expense_account, "account_currency")
-//                 .then(r => {
-//                     if (!r.message) return;
-//                     let account_currency = r.message.account_currency;
+        if (row.amount && row.expense_account) {
+            frappe.db.get_value("Account", row.expense_account, "account_currency")
+                .then(r => {
+                    if (!r.message) return;
+                    let account_currency = r.message.account_currency;
 
-//                     if (account_currency && account_currency !== company_currency) {
-//                         let rate = row.exchange_rate || 1;
-//                         frappe.model.set_value(cdt, cdn, "base_amount", row.amount * rate);
-//                     } else {
-//                         frappe.model.set_value(cdt, cdn, "base_amount", row.amount);
-//                     }
-//                 });
-//         }
-//     }
-// });
+                    if (account_currency && account_currency !== company_currency) {
+                        let rate = row.exchange_rate || 1;
+                        frappe.model.set_value(cdt, cdn, "base_amount", row.amount * rate);
+                    } else {
+                        frappe.model.set_value(cdt, cdn, "base_amount", row.amount);
+                    }
+                });
+        }
+    }
+});
