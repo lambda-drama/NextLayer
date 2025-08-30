a64af962693ab21f3d3489ac96966e6e94ff22a7
maniamartial <martialmania19@gmail.com>
2025-08-30 11:56:57 +0300

    feat: Shallow commit

--- nextlayer/www/frontend.py
+++ nextlayer/www/frontend.py
@@ -1,34 +1,34 @@
-import frappe
+# import frappe
 
-import json
-import re
+# import json
+# import re
 
-no_cache = 1
+# no_cache = 1
 
-SCRIPT_TAG_PATTERN = re.compile(r"\<script[^<]*\</script\>")
-CLOSING_SCRIPT_TAG_PATTERN = re.compile(r"</script\>")
+# SCRIPT_TAG_PATTERN = re.compile(r"\<script[^<]*\</script\>")
+# CLOSING_SCRIPT_TAG_PATTERN = re.compile(r"</script\>")
 
-def get_context(context):
-    # csrf_token = frappe.sessions.get_csrf_token()
-    # frappe.db.commit()
-    # context.csrf_token = csrf_token
+# def get_context(context):
+#     # csrf_token = frappe.sessions.get_csrf_token()
+#     # frappe.db.commit()
+#     # context.csrf_token = csrf_token
 
-    if frappe.session.user == "Guest":
-        boot = frappe.website.utils.get_boot_data()
-    else:
-        try:
-            boot = frappe.sessions.get()
-        except Exception as e:
-            raise frappe.SessionBootFailed from e
-    boot_json = frappe.as_json(boot, indent=None, separators=(",", ":"))
-    boot_json = SCRIPT_TAG_PATTERN.sub("", boot_json)
+#     if frappe.session.user == "Guest":
+#         boot = frappe.website.utils.get_boot_data()
+#     else:
+#         try:
+#             boot = frappe.sessions.get()
+#         except Exception as e:
+#             raise frappe.SessionBootFailed from e
+#     boot_json = frappe.as_json(boot, indent=None, separators=(",", ":"))
+#     boot_json = SCRIPT_TAG_PATTERN.sub("", boot_json)
 
-    boot_json = CLOSING_SCRIPT_TAG_PATTERN.sub("", boot_json)
-    boot_json = json.dumps(boot_json)
+#     boot_json = CLOSING_SCRIPT_TAG_PATTERN.sub("", boot_json)
+#     boot_json = json.dumps(boot_json)
 
-    context.update({
-        "build_version": frappe.utils.get_build_version(),
-        "boot": boot_json,
-    })
+#     context.update({
+#         "build_version": frappe.utils.get_build_version(),
+#         "boot": boot_json,
+#     })
 
-    return context
+#     return context
