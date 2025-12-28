9dbd7e549d1d9d721f9a88a49b75fddc18d75663
maniamartial <martialmania19@gmail.com>
2025-12-28 22:33:33 +0300

    feat: Update the print formats

---
+++ nextlayer/next_layer/utils/jinja_methods.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2025, jr@gmail.com and contributors
+# For license information, please see license.txt
+
+from nextlayer.next_layer.controllers.generate_qrcode import get_invoice_qr_code_url
+
+__all__ = ['get_invoice_qr_code_url']
+
