c15fa12abe04f8d9b7bf84a8626e390f0481dc94
maniamartial <martialmania19@gmail.com>
2026-03-28 15:23:29 +0300

    feat: Update the tokens on whatsap

--- nextlayer/next_layer/doctype/whatsapp_group_profile/whatsapp_group_profile_list.js
+++ nextlayer/next_layer/doctype/whatsapp_group_profile/whatsapp_group_profile_list.js
@@ -1,11 +1,35 @@
 // Copyright (c) 2026, jr@gmail.com and contributors
 // For license information, please see license.txt
 
+// frappe.listview_settings["Whatsapp Group Profile"] = {
+
+//     onload(listview) {
+
+//         listview.page.add_menu_item("Sync Group Profiles", () => {
+
+//             frappe.call({
+//                 method: "nextlayer.next_layer.api.wasender_whatsapp.sync_groups",
+//                 freeze: true,
+//                 freeze_message: "Syncing WhatsApp groups...",
+//                 callback() {
+//                     listview.refresh();
+//                 }
+//             });
+
+//         });
+
+//     }
+
+// };
+
+// Copyright (c) 2026, jr@gmail.com and contributors
+// For license information, please see license.txt
+
 frappe.listview_settings["Whatsapp Group Profile"] = {
 
     onload(listview) {
 
-        listview.page.add_menu_item("Sync Group Profiles", () => {
+        listview.page.add_inner_button("Sync Group Profiles", () => {
 
             frappe.call({
                 method: "nextlayer.next_layer.api.wasender_whatsapp.sync_groups",
